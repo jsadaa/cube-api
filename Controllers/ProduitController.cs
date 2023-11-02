c64adfa7f72296b93121e3cafae6ca1596aecf31
Léo Paillard <103212024+jsadaa@users.noreply.github.com>
2023-11-02 17:33:08 +0100

    Ajout controller produit

---
+++ Controllers/ProduitController.cs
@@ -0,0 +1,34 @@
+using ApiCube.DTOs;
+using ApiCube.DTOs.Responses;
+using ApiCube.Services.ProduitService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCube.Controllers;
+
+[Route("api/produits")]
+[ApiController]
+public class ProduitController : ControllerBase
+{
+    private readonly IProduitService _produitService;
+    
+    public ProduitController(IProduitService produitService)
+    {
+        _produitService = produitService;
+    }
+    
+    [HttpPost("ajouter")]
+    public IActionResult AjouterUnProduitAuStock([FromBody] AjouterProduitRequest ajouterProduit)
+    {
+        BaseResponse response = _produitService.AjouterUnProduitAuStock(ajouterProduit);
+        
+        return StatusCode(response.StatusCode, response.Data);
+    }
+    
+    [HttpGet("lister")]
+    public IActionResult ListerLesProduits()
+    {
+        BaseResponse response = _produitService.ListerLesProduits();
+        
+        return StatusCode(response.StatusCode, response.Data);
+    }
+}
