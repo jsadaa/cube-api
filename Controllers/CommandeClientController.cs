50e668a41ac5a25ce07f145c001a5832a9a9c529
Léo Paillard <103212024+jsadaa@users.noreply.github.com>
2024-03-30 23:58:46 +0100

    fix bad http code doc for commande client

--- Controllers/CommandeClientController.cs
+++ Controllers/CommandeClientController.cs
@@ -109,14 +109,12 @@ public class CommandeClientController : ControllerBase
     /// <param name="idProduit"></param>
     /// <returns></returns>
     /// <response code="200">produit_supprime_du_panier</response>
-    /// <response code="404">produit_introuvable | panier_client_introuvable</response>
-    /// <response code="409">produit_non_present_dans_panier</response>
+    /// <response code="404">produit_introuvable | panier_client_introuvable | produit_non_present_dans_panier</response>
     /// <response code="500">unexpected_error</response>
     [HttpDelete("panier/{idPanier:int}/produit/{idProduit:int}")]
     [ActionName("SupprimerUnProduitDuPanier")]
     [ProducesResponseType(typeof(string), 200)]
     [ProducesResponseType(typeof(ExpectedErrorResponse), 404)]
-    [ProducesResponseType(typeof(ExpectedErrorResponse), 409)]
     [ProducesResponseType(typeof(UnexpectedErrorResponse), 500)]
     [Produces("application/json")]
     public IActionResult SupprimerUnProduitDuPanier(int idPanier, int idProduit)
@@ -195,14 +193,14 @@ public class CommandeClientController : ControllerBase
     /// <param name="idPanier"></param>
     /// <returns></returns>
     /// <response code="200">panier_valide</response>
+    /// <response code="400">quantite_stock_insuffisant</response>
     /// <response code="404">panier_client_introuvable | client_introuvable | stock_introuvable | produit_introuvable</response>
-    /// <response code="409">quantite_stock_insuffisant</response>
     /// <response code="500">unexpected_error</response>
     [HttpPut("panier/{idPanier:int}/valider")]
     [ActionName("ValiderUnPanier")]
     [ProducesResponseType(typeof(string), 200)]
+    [ProducesResponseType(typeof(ExpectedErrorResponse), 400)]
     [ProducesResponseType(typeof(ExpectedErrorResponse), 404)]
-    [ProducesResponseType(typeof(ExpectedErrorResponse), 409)]
     [ProducesResponseType(typeof(UnexpectedErrorResponse), 500)]
     [Produces("application/json")]
     public IActionResult ValiderUnPanier(int idPanier)
